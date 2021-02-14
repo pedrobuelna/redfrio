7f53e500fdbc788764029d11d6f9cedd99d9de1b
f0c4 <foca_jr@hotmail.com>
2021-02-13 19:00:00 -0700

    Actualizacion notificaciones

--- src/app/notificaciones/notificaciones.page.ts
+++ src/app/notificaciones/notificaciones.page.ts
@@ -1,35 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { ModalController } from '@ionic/angular';
-import { MyModalPage } from '../modals/my-modal/my-modal.page';
-import { DatePipe } from '@angular/common';
-import {TaskService} from '../services/task.service';
-import { Notificacion } from '../interfaces/task';
+import {
+    Component,
+    OnInit
+} from '@angular/core';
+import {
+    Router
+} from '@angular/router';
+import {
+    ModalController
+} from '@ionic/angular';
+import {
+    MyModalPage
+} from '../modals/my-modal/my-modal.page';
+import {
+    DatePipe
+} from '@angular/common';
+import {
+    TaskService
+} from '../services/task.service';
+import {
+    Notificacion
+} from '../interfaces/task';
 import {
     NativeStorage
 } from '@ionic-native/native-storage/ngx';
 @Component({
-  selector: 'app-notificaciones',
-  templateUrl: './notificaciones.page.html',
-  styleUrls: ['./notificaciones.page.scss'],
-  providers: [DatePipe]
+    selector: 'app-notificaciones',
+    templateUrl: './notificaciones.page.html',
+    styleUrls: ['./notificaciones.page.scss'],
+    providers: [DatePipe]
 })
 export class NotificacionesPage implements OnInit {
-  dataReturned: any;
-  tasks: Notificacion[] = [];
-  notificaciones:any;
-  cantidadNot:any;
-  constructor(
-    private router: Router,
-    private datePipe: DatePipe,
-    private taskService: TaskService,
-    public modalController: ModalController,
-    private nativeStorage: NativeStorage) { 
-      
+    dataReturned: any;
+    tasks: Notificacion[] = [];
+    notificaciones: any;
+    cantidadNot: any;
+    constructor(
+        private router: Router,
+        private datePipe: DatePipe,
+        private taskService: TaskService,
+        public modalController: ModalController,
+        private nativeStorage: NativeStorage) {
+
     }
-    currentDate:any  = new Date();
-    uuid_cliente:any = "8e96af95-4575-47e9-a2aa-b56aba2f035f";
-    uuid_notificacion:any;
+    currentDate: any = new Date();
+    uuid_cliente: any = "8e96af95-4575-47e9-a2aa-b56aba2f035f";
+    uuid_notificacion: any;
     listas: any;
     // lista: Array<any> = [
     //   {
@@ -47,118 +62,139 @@ export class NotificacionesPage implements OnInit {
     //     estatus:0
     //   },
     //]
-    
+
     async openModal(index) {
-      this.uuid_notificacion = this.listas[index].uuid_notificacion
-      
-      const modal = await this.modalController.create({
-        component: MyModalPage,
-        componentProps: {
-          "titulo": this.listas[index].titulo,
-          "descripcion": this.listas[index].descripcion,
-          "url_imagen": this.listas[index].url_imagen,
-          "fecha": this.listas[index].fecha,
-          "uuid_cliente": this.uuid_cliente,
-          "uuid_notificacion": this.listas[index].uuid_notificacion,
-          "status": this.listas[index].status,
-        }
-        
-      });
-      const Notificacion = {
-        status: "1"
-      };
-      this.taskService.updateNotificacion(Notificacion,this.uuid_cliente,this.uuid_notificacion)
-      .subscribe(listas => {
-        this.taskService.getNotificaciones()
-        .subscribe(listas => {
-            this.listas = listas;
+        this.uuid_notificacion = this.listas[index].uuid_notificacion
+
+        const modal = await this.modalController.create({
+            component: MyModalPage,
+            componentProps: {
+                "titulo": this.listas[index].titulo,
+                "descripcion": this.listas[index].descripcion,
+                "url_imagen": this.listas[index].url_imagen,
+                "fecha": this.listas[index].fecha,
+                "uuid_cliente": this.uuid_cliente,
+                "uuid_notificacion": this.listas[index].uuid_notificacion,
+                "status": this.listas[index].status,
+            }
+
         });
-        this.taskService.getNotificacionesNoLeidas()
-      .subscribe(notificaciones => {
-          this.notificaciones = notificaciones;
-          this.cantidadNot = this.notificaciones.length
+        const Notificacion = {
+            status: "1"
+        };
+        this.taskService.updateNotificacion(Notificacion, this.uuid_cliente, this.uuid_notificacion)
+            .subscribe(listas => {
+                this.nativeStorage.getItem('app')
+                    .then(
+                        app => {
+                            console.log("==APP DATA==");
+                            console.log(app);
+                            console.log("uuid_cliente: " + app.uuid_cliente);
+                            this.taskService.getNotificaciones(app.uuid_cliente)
+                                .subscribe(listas => {
+                                    this.listas = listas;
+                                    this.cantidadNot = this.listas.length
+                                    //ok pedro
+                                });
+                        },
+                        error => console.error("NO HAY UUID_CLIENTE")
+                    );
+                var fecha = new Date();
+                var options = {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric'
+                };
+                this.currentDate = fecha.toLocaleDateString("es-ES", options)
 
-      });
-        var fecha = new Date();
-        var options = { year: 'numeric', month: 'long', day: 'numeric' };
-        this.currentDate = fecha.toLocaleDateString("es-ES", options)
+            });
 
-      });
-      
-      modal.onDidDismiss().then((dataReturned) => {
-        if (dataReturned !== null) {
-          this.dataReturned = dataReturned.data;
-          //alert('Modal Sent Data :'+ dataReturned);
-        }
-      });
-      
-      return await modal.present();
-      
+        modal.onDidDismiss().then((dataReturned) => {
+            if (dataReturned !== null) {
+                this.dataReturned = dataReturned.data;
+                //alert('Modal Sent Data :'+ dataReturned);
+            }
+        });
+
+        return await modal.present();
+
     }
-  ngOnInit() {
-    // this.taskService.getNotificacionesNoLeidas()
-    //   .subscribe(notificaciones => {
-    //       this.notificaciones = notificaciones;
-    //       this.cantidadNot = this.notificaciones.length
-          
-    //   });
-    this.nativeStorage.getItem('app')
-    .then(
-        app => {
-            console.log("==APP DATA==");
-            console.log(app);
-            console.log("uuid_cliente: "+app.uuid_cliente);
-            this.taskService.getNotificaciones(app.uuid_cliente)
+    ngOnInit() {
+        // this.taskService.getNotificacionesNoLeidas()
+        //   .subscribe(notificaciones => {
+        //       this.notificaciones = notificaciones;
+        //       this.cantidadNot = this.notificaciones.length
+
+        //   });
+        var fecha = new Date();
+        var options = {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        };
+        this.currentDate = fecha.toLocaleDateString("es-ES", options)
+        this.nativeStorage.getItem('app')
+            .then(
+                app => {
+                    console.log("==APP DATA==");
+                    console.log(app);
+                    console.log("uuid_cliente: " + app.uuid_cliente);
+                    this.taskService.getNotificaciones(app.uuid_cliente)
+                        .subscribe(listas => {
+                            this.listas = listas;
+                            this.cantidadNot = this.listas.length
+                            //ok pedro
+                        });
+                },
+                error => console.error("NO HAY UUID_CLIENTE")
+            );
+
+        // var fecha = new Date();
+        //     var options = { year: 'numeric', month: 'long', day: 'numeric' };
+        //     console.log(
+        //       fecha.toLocaleDateString("es-ES", options)
+        //     );
+        //     this.currentDate = fecha.toLocaleDateString("es-ES", options)
+    }
+    ionViewWillEnter() {
+        // this.taskService.getNotificacionesNoLeidas()
+        //   .subscribe(notificaciones => {
+        //       this.notificaciones = notificaciones;
+        //       this.cantidadNot = this.notificaciones.length
+        //   });
+    }
+    onclickNotificaciones() {
+        this.taskService.getNotificaciones()
             .subscribe(listas => {
                 this.listas = listas;
-                //ok pedro
             });
-        },
-        error => console.error("NO HAY UUID_CLIENTE")
-    );
-    
-    // var fecha = new Date();
-    //     var options = { year: 'numeric', month: 'long', day: 'numeric' };
-    //     console.log(
-    //       fecha.toLocaleDateString("es-ES", options)
-    //     );
-    //     this.currentDate = fecha.toLocaleDateString("es-ES", options)
-  }
-  ionViewWillEnter(){
-    // this.taskService.getNotificacionesNoLeidas()
-    //   .subscribe(notificaciones => {
-    //       this.notificaciones = notificaciones;
-    //       this.cantidadNot = this.notificaciones.length
-    //   });
-  }
-  onclickNotificaciones(){
-    this.taskService.getNotificaciones()
-      .subscribe(listas => {
-          this.listas = listas;
-    });
-    this.taskService.getNotificacionesNoLeidas()
-      .subscribe(notificaciones => {
-          this.notificaciones = notificaciones;
-          this.cantidadNot = this.notificaciones.length
-      });
-      var fecha = new Date();
-      var options = { year: 'numeric', month: 'long', day: 'numeric' };
-      console.log(
-        fecha.toLocaleDateString("es-ES", options)
-      );
-      this.currentDate = fecha.toLocaleDateString("es-ES", options)
-    this.router.navigate(['/notificaciones']);
-  }
-  onclickUbicaciones(){
-    this.router.navigate(['/ubicaciones']);
-  }
-  onclickMenu(){
-    this.router.navigate(['/principal']);
-  }
-  onclickCategorias(){
-    this.router.navigate(['/categorias']);
-  }
-  onclickUsuario(){
-    this.router.navigate(['/editarperfil']);
-  }
+        this.taskService.getNotificacionesNoLeidas()
+            .subscribe(notificaciones => {
+                this.notificaciones = notificaciones;
+                this.cantidadNot = this.notificaciones.length
+            });
+        var fecha = new Date();
+        var options = {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        };
+        console.log(
+            fecha.toLocaleDateString("es-ES", options)
+        );
+        this.currentDate = fecha.toLocaleDateString("es-ES", options)
+        this.router.navigate(['/notificaciones']);
+    }
+    onclickUbicaciones() {
+        this.router.navigate(['/ubicaciones']);
+    }
+    onclickMenu() {
+        this.router.navigate(['/principal']);
+    }
+    onclickCategorias() {
+        this.router.navigate(['/categorias']);
+    }
+    onclickUsuario() {
+        this.router.navigate(['/editarperfil']);
+    }
 }
